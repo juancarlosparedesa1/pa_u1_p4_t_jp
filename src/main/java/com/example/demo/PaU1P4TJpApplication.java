c6f441b3fb6c2a93de7f6fa6cc80fe5e26530366
juancarlosparedesa1 <jcparedesa1@uce.edu.ec>
2023-05-15 15:13:02 -0500

    subida tarea3

---
+++ src/main/java/com/example/demo/PaU1P4TJpApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaU1P4TJpApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaU1P4TJpApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Este proyecto sera clonado y guardado en un nuevo branch");
+	}
+
+}
